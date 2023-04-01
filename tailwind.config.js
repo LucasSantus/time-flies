0651aec868ac1587194da6a88f5c37bd57c9f0ed
Lucas Santos <leos.developer@gmail.com>
2023-04-01 14:48:05 -0300

    :pushpin: fix tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ['./*/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    // Or if using `src` directory:
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     screens: {
       xs: '425px',
