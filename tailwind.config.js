3989d5a1f3092b309e44ce75e2c80a6712106bc0
Lucas Santos <leos.developer@gmail.com>
2023-04-01 21:22:52 -0300

    :rocket: add new config of colors and container

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,34 @@ module.exports = {
       xs: '425px',
       ...defaultTheme.screens,
     },
-    extend: {},
+    extend: {
+      colors: {
+        custom: {
+          green: {
+            800: '#015F43',
+            500: '#00875F',
+            300: '#00B37E',
+          },
+          red: {
+            800: '#00875F',
+            500: '#F03847',
+          },
+          gray: {
+            900: '#121214',
+            800: '#202024',
+            700: '#323238',
+            600: '#7C7C8A',
+            500: '#8D8D99',
+            400: '#C4C4CC',
+            300: '#E1E1E6',
+          },
+        },
+      },
+      container: {
+        center: true,
+        padding: '1rem',
+      },
+    },
   },
   plugins: [],
 };
