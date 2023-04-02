dd0ed7d12ddecec1c627eb141b725523b37b7567
Lucas Santos <leos.developer@gmail.com>
2023-04-01 21:23:19 -0300

    :wastebasket: remove desnecessary files

--- src/components/TimerNumber.tsx
+++
@@ -1,7 +0,0 @@
-interface ITimerNumberProps {
-  number: string;
-}
-
-export const TimerNumber: React.FC<ITimerNumberProps> = ({ number }) => {
-  return <div className="p-1 text-9xl h-full rounded">{number}</div>;
-};
