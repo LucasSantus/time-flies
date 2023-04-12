ace706052bc6d8e1b5baa663830cf66754256767
Lucas Santos <leos.developer@gmail.com>
2023-04-12 00:36:48 -0300

    :pushpin: add new configs in file tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,43 +1,44 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
+  darkMode: "class",
   content: [
     // Or if using `src` directory:
-    './src/**/*.{js,ts,jsx,tsx}',
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     screens: {
-      xs: '425px',
+      xs: "425px",
       ...defaultTheme.screens,
     },
     extend: {
       colors: {
         custom: {
           green: {
-            800: '#015F43',
-            500: '#00875F',
-            300: '#00B37E',
+            800: "#015F43",
+            500: "#00875F",
+            300: "#00B37E",
           },
           red: {
-            800: '#00875F',
-            500: '#F03847',
+            800: "#00875F",
+            500: "#F03847",
           },
           gray: {
-            900: '#121214',
-            800: '#202024',
-            700: '#323238',
-            600: '#7C7C8A',
-            500: '#8D8D99',
-            400: '#C4C4CC',
-            300: '#E1E1E6',
+            800: "#121214",
+            700: "#202024",
+            600: "#323238",
+            500: "#29292E",
+            400: "#7C7C8A",
+            300: "#8D8D99",
+            200: "#C4C4CC",
+            100: "#E1E1E6",
           },
         },
       },
       container: {
         center: true,
-        padding: '1rem',
+        padding: "1rem",
       },
     },
   },
