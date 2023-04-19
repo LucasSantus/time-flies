2992661d2372a3ddc4ec3fc2efe948efcd39634c
Lucas Santos <leos.developer@gmail.com>
2023-04-19 00:08:37 -0300

    :rocket: create key of locales

--- src/utils/locales.ts
+++ src/utils/locales.ts
@@ -1,16 +1,17 @@
+import { DEFAULT_LOCALE, KEY_LOCALE_COOKIE } from "@/contants/globals";
 import generalTranslations, { Locale } from "@/locales/general";
 import { getCookieInBrowser, setCookieInBrowser } from "./cookies";
 
 export function setLocaleFromCookie(locale: string) {
-  setCookieInBrowser("locale", locale);
+  setCookieInBrowser(KEY_LOCALE_COOKIE, locale);
 }
 
 export function getLocaleFromCookie(): Locale {
-  const locale = getCookieInBrowser("locale");
+  const locale = getCookieInBrowser(KEY_LOCALE_COOKIE);
 
   if (locale && generalTranslations[locale as Locale]) {
     return locale as Locale;
   }
 
-  return "en-US";
+  return DEFAULT_LOCALE;
 }
