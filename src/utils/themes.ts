1fdf21917a5c15cfbd4612f6786c007ec7ddd03f
Lucas Santos <leos.developer@gmail.com>
2023-04-19 00:08:25 -0300

    :rocket: create key of theme

--- src/utils/themes.ts
+++ src/utils/themes.ts
@@ -1,12 +1,13 @@
-import { DEFAULT_THEME, DEFAULT_THEME_NAME, ITheme, themes } from "@/contants/themes";
+import { DEFAULT_THEME, DEFAULT_THEME_NAME, KEY_THEME_COOKIE } from "@/contants/globals";
+import { ITheme, themes } from "@/contants/themes";
 import { getCookieInBrowser, setCookieInBrowser } from "./cookies";
 
 export function setThemeNameFromCookie(theme: string) {
-  setCookieInBrowser("theme", theme);
+  setCookieInBrowser(KEY_THEME_COOKIE, theme);
 }
 
 export function getThemeNameFromCookie(): string {
-  const theme = getCookieInBrowser("theme");
+  const theme = getCookieInBrowser(KEY_THEME_COOKIE);
 
   if (theme) return theme;
 
