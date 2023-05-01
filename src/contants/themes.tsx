7c36f66f14b6e212a139971596a09afc5796acd7
Lucas Santos <leos.developer@gmail.com>
2023-05-01 00:06:21 -0300

    :pushpin: fix types

--- src/contants/themes.tsx
+++ src/contants/themes.tsx
@@ -1,7 +1,7 @@
-import { ISelectorTheme } from "@/types/Themes";
+import { IThemeSelector } from "@/types/Themes";
 import { Moon, Sun } from "phosphor-react";
 
-export const THEME_MODES: ISelectorTheme[] = [
+export const THEME_MODES: IThemeSelector[] = [
   {
     title: "light",
     icon: <Sun size={23} className="text-slate-400 dark:text-custom-gray-400" />,
