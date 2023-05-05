a94424cab412f8a903d74bfe9587824aaea9eb1b
Lucas Santos <leos.developer@gmail.com>
2023-05-05 19:43:44 -0300

    :rocket: add responsively in page screen home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Countdown } from "@/components/Countdown";
+import { ThemeSelector } from "@/components/ThemeSelector";
 import { Contexts } from "@/context";
 import { ToastContainer } from "react-toastify";
 
@@ -18,15 +19,15 @@ export default function Home() {
         pauseOnHover
         theme="colored"
       />
-      <div className="flex flex-col">
-        {/* <div className="flex justify-end">
+      <div className="flex flex-col md:h-screen">
+        <div className="flex sm:justify-end">
           <div className="m-5 flex gap-2">
             <ThemeSelector />
           </div>
-        </div> */}
+        </div>
 
         {/* TODO: Centralizar corretamente o componente abaixo */}
-        <div className="flex items-center justify-center">
+        <div className="flex h-full w-full items-start justify-center sm:items-center">
           <Countdown />
         </div>
       </div>
