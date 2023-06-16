c6c38bbe901e2567d90e7eb91813056d240b6500
Lucas Santos <leos.developer@gmail.com>
2023-06-16 19:09:38 -0300

    :pushpin: add no script in project

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,26 +1,34 @@
 "use client";
 
+import { easeInOutAnimationVerticalDislocate } from "@/utils/animation/easeInOutAnimationVerticalDislocate";
+import { motion } from "framer-motion";
 import Link from "next/link";
-
-export const metadata = {
-  title: "Time Flies",
-  description: "A simple countdown timer with a beautiful UI",
-};
 
 export default function NotFound() {
   return (
     <div className="flex h-screen items-center justify-center">
       <section className="flex flex-col items-center justify-center gap-2 rounded-lg">
-        <span className="text-9xl font-bold text-slate-200">404</span>
-        <span className="text-xl font-medium text-slate-200">
-          A página solicitada não foi encontrada!
-        </span>
-        <Link
-          href="/"
-          className="mt-4 text-blue-500 underline hover:text-blue-400"
+        <motion.span
+          {...easeInOutAnimationVerticalDislocate({ delay: 0.3 })}
+          className="text-9xl font-bold text-slate-200"
         >
-          Ir para tela principal
-        </Link>
+          404
+        </motion.span>
+        <motion.span
+          {...easeInOutAnimationVerticalDislocate({ delay: 0.6 })}
+          className="text-xl font-medium text-slate-200"
+        >
+          A página solicitada não foi encontrada!
+        </motion.span>
+
+        <motion.div {...easeInOutAnimationVerticalDislocate({ delay: 0.9 })}>
+          <Link
+            href="/"
+            className="mt-4 text-blue-500 underline hover:text-blue-400"
+          >
+            Ir para a tela principal
+          </Link>
+        </motion.div>
       </section>
     </div>
   );
