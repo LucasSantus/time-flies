a8d384a792e298eb2eee097a2b72054a66d91c91
Lucas Santos <leos.developer@gmail.com>
2023-06-16 00:19:32 -0300

    :pushpin: adapt code of Modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,48 +1,58 @@
 "use client";
 
-import { useDialog } from "@/hooks/useDialog";
+import { useModal } from "@/hooks/useModal";
 import { easeInOutAnimationVerticalDislocate } from "@/utils/animation/easeInOutAnimationVerticalDislocate";
-import * as Dialog from "@radix-ui/react-dialog";
+import * as DialogPrimitive from "@radix-ui/react-dialog";
 import { motion } from "framer-motion";
 import { X } from "lucide-react";
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useEffect } from "react";
 
 interface ModalProps extends PropsWithChildren {
-  title: string;
-  trigger: React.ReactNode;
+  title?: string;
+  trigger?: React.ReactNode;
+  isOpenDefault?: boolean;
 }
 
-export const Modal: React.FC<ModalProps> = ({ title, trigger, children }) => {
-  const { isOpen, setIsOpen } = useDialog();
+export const Modal: React.FC<ModalProps> = ({
+  title,
+  trigger,
+  isOpenDefault,
+  children,
+}) => {
+  const { isOpen, setIsOpen } = useModal();
+
+  useEffect(() => {
+    if (isOpenDefault) setIsOpen(isOpenDefault);
+  }, [isOpenDefault, setIsOpen]);
 
   return (
-    <Dialog.Root open={isOpen} onOpenChange={setIsOpen}>
-      <Dialog.Trigger asChild>{trigger}</Dialog.Trigger>
-      <Dialog.Portal>
+    <DialogPrimitive.Root open={isOpen} onOpenChange={setIsOpen}>
+      <DialogPrimitive.Trigger asChild>{trigger}</DialogPrimitive.Trigger>
+      <DialogPrimitive.Portal>
         <motion.div
           {...easeInOutAnimationVerticalDislocate({ delay: 0.7 })}
           className="fixed inset-0 flex items-center justify-center bg-black/50"
         >
-          <Dialog.Content className="data-[state=open] w-full max-w-md transform overflow-hidden rounded-md bg-slate-100 p-4 text-left align-middle shadow-xl transition-all dark:bg-custom-gray-700">
+          <DialogPrimitive.Content className="data-[state=open] w-full max-w-md transform overflow-hidden rounded-md bg-slate-100 p-4 text-left align-middle shadow-xl transition-all dark:bg-custom-gray-700">
             <div className="flex items-start justify-between ">
-              <Dialog.Title className="text-lg font-medium leading-6 text-gray-900 dark:text-custom-gray-100">
+              <DialogPrimitive.Title className="text-lg font-medium leading-6 text-gray-900 dark:text-custom-gray-100">
                 {title}
-              </Dialog.Title>
+              </DialogPrimitive.Title>
 
-              <Dialog.Close asChild>
+              <DialogPrimitive.Close asChild>
                 <button
                   className="flex h-6 w-6 appearance-none items-center justify-center"
                   aria-label="Close Modal"
                 >
                   <X className="h-5 w-5 stroke-gray-500 hover:opacity-70" />
                 </button>
-              </Dialog.Close>
+              </DialogPrimitive.Close>
             </div>
 
             {children}
-          </Dialog.Content>
+          </DialogPrimitive.Content>
         </motion.div>
-      </Dialog.Portal>
-    </Dialog.Root>
+      </DialogPrimitive.Portal>
+    </DialogPrimitive.Root>
   );
 };
