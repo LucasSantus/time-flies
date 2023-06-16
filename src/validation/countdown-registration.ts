72c216c972b132d7a2f52d8c7545db32c53e9ae6
Lucas Santos <leos.developer@gmail.com>
2023-06-15 23:58:57 -0300

    :pushpin: update registration countdown of zod in form

--- src/validation/countdown-registration.ts
+++ src/validation/countdown-registration.ts
@@ -1,25 +1,50 @@
+import { formatNumberToSeparatedTimes } from "@/utils/formatNumberToSeparatedTimes";
 import { z } from "zod";
+
+const MIN_VALUE = 0;
+const MIN_MESSAGE = "Insira valor maior que 0";
+
+const MAX_VALUE = 9;
+const MAX_MESSAGE = "Insira valor menor que 9";
 
 export const countdownFormSchema = z
   .object({
-    hourLeft: z.coerce.number(),
-    hourRight: z.coerce.number(),
-    minuteLeft: z.coerce.number(),
-    minuteRight: z.coerce.number(),
-    secondLeft: z.coerce.number(),
-    secondRight: z.coerce.number(),
+    hourLeft: z.coerce
+      .number()
+      .min(MIN_VALUE, MIN_MESSAGE)
+      .max(MAX_VALUE, MAX_MESSAGE),
+    hourRight: z.coerce
+      .number()
+      .min(MIN_VALUE, MIN_MESSAGE)
+      .max(MAX_VALUE, MAX_MESSAGE),
+    minuteLeft: z.coerce
+      .number()
+      .min(MIN_VALUE, MIN_MESSAGE)
+      .max(MAX_VALUE, MAX_MESSAGE),
+    minuteRight: z.coerce
+      .number()
+      .min(MIN_VALUE, MIN_MESSAGE)
+      .max(MAX_VALUE, MAX_MESSAGE),
+    secondLeft: z.coerce
+      .number()
+      .min(MIN_VALUE, MIN_MESSAGE)
+      .max(MAX_VALUE, MAX_MESSAGE),
+    secondRight: z.coerce
+      .number()
+      .min(MIN_VALUE, MIN_MESSAGE)
+      .max(MAX_VALUE, MAX_MESSAGE),
   })
   .refine(({ hourLeft, hourRight }) => {
-    const hour = Number(`${hourLeft}${hourRight}`);
+    const hour = formatNumberToSeparatedTimes(hourLeft, hourRight);
     return hour >= 0 && hour <= 23;
-  }, "Horário inválido!")
+  }, "As horas inseridas são inválidas!")
   .refine(({ minuteLeft, minuteRight }) => {
-    const minute = Number(`${minuteLeft}${minuteRight}`);
+    const minute = formatNumberToSeparatedTimes(minuteLeft, minuteRight);
     return minute >= 0 && minute <= 59;
-  }, "Minuto inválido!")
+  }, "Os minutos inseridos são inválidos!")
   .refine(({ secondLeft, secondRight }) => {
-    const second = Number(`${secondLeft}${secondRight}`);
+    const second = formatNumberToSeparatedTimes(secondLeft, secondRight);
     return second >= 0 && second <= 59;
-  }, "Segundo inválido!");
+  }, "Os segundos inseridos são inválidos!");
 
 export type CountdownFormData = z.infer<typeof countdownFormSchema>;
