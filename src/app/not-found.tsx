217b6e672ea2b4a1dd8772c54057e76aa6036475
Lucas Santos <leos.developer@gmail.com>
2024-04-28 23:07:47 -0300

    :rocket: feat: create not found page global

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,35 +1,19 @@
-"use client";
-
-import { easeInOutAnimationVerticalDislocate } from "@/utils/animation/easeInOutAnimationVerticalDislocate";
-import { motion } from "framer-motion";
-import Link from "next/link";
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 
 export default function NotFound() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <section className="flex flex-col items-center justify-center gap-2 rounded-lg">
-        <motion.span
-          {...easeInOutAnimationVerticalDislocate({ delay: 0.3 })}
-          className="text-9xl font-bold text-slate-200"
-        >
-          404
-        </motion.span>
-        <motion.span
-          {...easeInOutAnimationVerticalDislocate({ delay: 0.6 })}
-          className="text-xl font-medium text-slate-200"
-        >
-          A página solicitada não foi encontrada!
-        </motion.span>
-
-        <motion.div {...easeInOutAnimationVerticalDislocate({ delay: 0.9 })}>
-          <Link
-            href="/"
-            className="mt-4 text-blue-500 underline hover:text-blue-400"
-          >
-            Ir para a tela principal
-          </Link>
-        </motion.div>
-      </section>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-9xl font-bold text-emerald-500">404</h1>
+      <h2 className="text-3xl font-medium text-gray-400 mb-4">
+        Página não encontrada
+      </h2>
+      <Link href="/">
+        <Button>
+          {/* <HiArrowNarrowLeft size={20} /> */}
+          Voltar para Home
+        </Button>
+      </Link>
     </div>
-  );
+  )
 }
